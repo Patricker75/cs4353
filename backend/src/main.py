a222bcfb1ffdab8268c2eca7900a13860560d5ff
Patricker75 <biet.vui@gmail.com>
2023-09-05 13:16:19 -0500

    added base flask route

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,1 +1,12 @@
-print('hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def base():
+    response = {
+        "message": 'Hello from server'
+    }
+
+    return response
