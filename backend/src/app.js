2c4099ac3982ef8769eb8f4e6ddc1f4cf604f103
TsmHorns <cjentushorns@gmail.com>
2023-10-13 17:16:26 -0500

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,17 +9,6 @@ const app = express();
 app.use(json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-// Register the auth router
-app.use('/auth', authRouter);
-
-// Register the profile router
-app.use('/profile', profileRouter);
-
-// Register the quotes router
-app.use('/quotes', quotesRouter);
 for (let index in routes) {
   app.use(routes[index])
 }
