a99fa3ce1aa47fa22186207f64be1bd17e15730e
TsmHorns <cjentushorns@gmail.com>
2023-10-13 18:50:07 -0500

    profile backend works look in console

--- frontend/src/components/ClientProfile.js
+++ frontend/src/components/ClientProfile.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import clientProfileSlice from '../redux/clientProfileSlice';
 
 import {
+  updateUserID,
   updateName,
   updateMainAddress,
   updateAuxAddress,
@@ -66,19 +67,29 @@ export const ClientProfile = () => {
     'Wisconsin',
     'Wyoming'
   ];
-
   // Rest of your component code
   // ...
-
-
 
 
   const dispatch = useDispatch();
   const clientProfile = useSelector((state) => state.clientProfile);
 
+  const generateRandomUserId = () => {
+    return 'user_' + new Date().getTime() + '_' + Math.floor(Math.random() * 1000);
+  };
+  useEffect(() => {
+    // Generate a random user ID only when the component first loads
+    const userId = generateRandomUserId();
+    // Dispatch the user ID to the Redux store
+    dispatch(updateUserID(userId));
+  }, []); // Empty dependency array to run this effect only once
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
     evt.stopPropagation();
+
+    // Generate a random user ID
+    const userId = generateRandomUserId();
 
     // Dispatch actions to update the client profile fields
     dispatch(updateName(clientProfile.name));
@@ -87,33 +98,11 @@ export const ClientProfile = () => {
     dispatch(updateCity(clientProfile.city));
     dispatch(updateState(clientProfile.state));
     dispatch(updateZipcode(clientProfile.zipcode));
-
-    let newProfile = {
-      name: clientProfile.name,
-      mainAddress: clientProfile.mainAddress,
-      auxAddress: clientProfile.auxAddress,
-      city: clientProfile.city,
-      state: clientProfile.state,
-      zipcode: clientProfile.zipcode,
-    };
-/*
-    axios
-      .put('/api/client/profile', newProfile)
-      .then((response) => {
-        if (response.status === 200) {
-          console.log('Profile updated successfully on the server.');
-        } else {
-          console.error('Failed to update profile on the server.');
-        }
-      })
-      .catch((error) => {
-        console.error('An error occurred while updating the profile:', error);
-      });
-      */
   };
 
-  const handleSave = (evt) => {
+  const handleSave = () => {
     console.log('Profile data stored in Redux:', {
+      userID: clientProfile.userID,
       name: clientProfile.name,
       mainAddress: clientProfile.mainAddress,
       auxAddress: clientProfile.auxAddress,
@@ -121,15 +110,24 @@ export const ClientProfile = () => {
       state: clientProfile.state,
       zipcode: clientProfile.zipcode,
     });
-/*
-    axios
-      .put('http://localhost:4001/profile', {
+
+    let newProfile = {
+      userID: clientProfile.userID,
+      profileData: {
         name: clientProfile.name,
         mainAddress: clientProfile.mainAddress,
         auxAddress: clientProfile.auxAddress,
         city: clientProfile.city,
         state: clientProfile.state,
         zipcode: clientProfile.zipcode,
+      },
+    };
+
+    axios
+      .put('http://localhost:4001/api/profile', newProfile, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
       })
       .then((response) => {
         if (response.status === 200) {
@@ -141,7 +139,6 @@ export const ClientProfile = () => {
       .catch((error) => {
         console.error('An error occurred while updating the profile:', error);
       });
-      */
   };
 
   return (
@@ -237,4 +234,3 @@ export const ClientProfile = () => {
     </div>
   );
 };
-export default ClientProfile;
