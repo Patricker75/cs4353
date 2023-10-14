6841ce4d4dcbfb552e100d56d5109043ae358ac6
Patricker75 <biet.vui@gmail.com>
2023-10-13 23:21:55 -0500

    fixed unit tests

--- backend/src/tests/profile.test.js
+++ backend/src/tests/profile.test.js
@@ -1,64 +1,111 @@
-import { handleProfileUpdate } from "../handlers/profileHandler";
+import {
+  handleGetProfile,
+  handleProfileUpdate,
+} from "../handlers/profileHandler";
 
-describe('Test Profile Handlers', () => {
-  test('Test profile update - valid', () => {
+describe("Test Profile Handlers", () => {
+  test("Test profile update - valid", () => {
     const req = {
       body: {
-        userID: 'john.doe@example.com',
+        userID: "john.doe@example.com",
         profileData: {
-          name: 'John Doe',
-          mainAddress: '123 City St.',
-          auxAddress: '456 Town Rd.',
-          city: 'Houston',
-          state: 'TX',
-          zipcode: '12345',
-        }
-      }
-    }
+          name: "John Doe",
+          mainAddress: "123 City St.",
+          auxAddress: "456 Town Rd.",
+          city: "Houston",
+          state: "TX",
+          zipcode: "12345",
+        },
+      },
+    };
     const res = {
       data: {},
       code: 0,
 
-      status: function(status) {
-        this.code = status
+      status: function (status) {
+        this.code = status;
       },
-      send: function(input) {
-        this.data = input
-      }
-    }
+      send: function (input) {
+        this.data = input;
+      },
+    };
 
-    handleProfileUpdate(req, res)
+    handleProfileUpdate(req, res);
 
-    expect(res.data.message).toEqual('Profile updated successfully')
-  })
+    expect(res.data.message).toEqual("Profile updated successfully");
+  });
 
-  test('Test profile update - invalid', () => {
+  test("Test profile update - invalid", () => {
     const req = {
-      body: {
-        profileData: {
-          name: 'John Doe',
-          mainAddress: '123 City St.',
-          auxAddress: '456 Town Rd.',
-          city: 'Houston',
-          state: 'TX',
-          zipcode: '12345',
-        }
-      }
-    }
+      body: {},
+    };
     const res = {
       data: {},
       code: 0,
 
-      status: function(status) {
-        this.code = status
+      status: function (status) {
+        this.code = status;
       },
-      send: function(input) {
-        this.data = input
-      }
-    }
+      send: function (input) {
+        this.data = input;
+      },
+    };
 
-    handleProfileUpdate(req, res)
+    handleProfileUpdate(req, res);
 
-    expect(res.data.message).toEqual('Profile updated successfully')
-  })
-})
+    expect(res.data.error).toEqual("User ID not provided in the request.");
+  });
+
+  test("Test profile get - valid", () => {
+    const req = {
+      body: {
+        userID: "john.doe@example.com",
+      },
+    };
+    const res = {
+      data: {},
+      code: 0,
+
+      status: function (status) {
+        this.code = status;
+      },
+      send: function (input) {
+        this.data = input;
+      },
+    };
+
+    handleGetProfile(req, res);
+
+    let profileData = {
+      name: "John Doe",
+      mainAddress: "123 City St.",
+      auxAddress: "456 Town Rd.",
+      city: "Houston",
+      state: "TX",
+      zipcode: "12345",
+    };
+
+    expect(res.data).toEqual(profileData);
+  });
+
+  test("Test profile get - invalid", () => {
+    const req = {
+      body: {},
+    };
+    const res = {
+      data: {},
+      code: 0,
+
+      status: function (status) {
+        this.code = status;
+      },
+      send: function (input) {
+        this.data = input;
+      },
+    };
+
+    handleGetProfile(req, res);
+
+    expect(res.data.error).toEqual("User profile not found.");
+  });
+});
--- backend/src/tests/quotes.test.js
+++ backend/src/tests/quotes.test.js
@@ -1,70 +1,122 @@
-import { handleNewQuote, handleGetHistory } from "../handlers/quotesHandler"
+import { handleNewQuote, handleGetHistory } from "../handlers/quotesHandler";
 
-describe('Test Quotes Handler', () => {
-  test('Test Get Quote History - empty', () => {
-    const req = {}
+describe("Test Quotes Handler", () => {
+  test("Test Get Quote History - empty", () => {
+    const req = {};
     const res = {
       data: {},
       code: 0,
 
-      status: function(status) {
-        this.code = status
+      status: function (status) {
+        this.code = status;
       },
-      send: function(input) {
-        this.data = input
-      }
-    }
+      send: function (input) {
+        this.data = input;
+      },
+    };
 
-    handleGetHistory(req, res)
-    
-    expect(res.data).toEqual([])
-  })
+    handleGetHistory(req, res);
 
-  test('Test New Quote - valid', () => {
+    expect(res.data).toEqual([]);
+  });
+
+  test("Test New Quote - valid", () => {
     const req = {
       body: {
-        text: 'New Fuel'
-      }
-    }
+        userID: 1,
+        requestData: {
+          amount: 10,
+          unitPrice: 1,
+          deliveryDate: "2023-01-01",
+          mainAddress: "123 City St.",
+        },
+      },
+    };
     const res = {
       data: {},
       code: 0,
 
-      status: function(status) {
-        this.code = status
+      status: function (status) {
+        this.code = status;
       },
-      send: function(input) {
-        this.data = input
-      }
-    }
+      send: function (input) {
+        this.data = input;
+      },
+    };
 
-    handleNewQuote(req, res)
-    
-    expect(res.data.message).toEqual('New fuel quote created')
-  })
+    handleNewQuote(req, res);
 
-  test('Test Get Quote History - empty', () => {
-    const req = {}
+    expect(res.data.message).toEqual("New fuel data stored");
+  });
+
+  test("Test New Quote - no user id", () => {
+    const req = {
+      body: {
+        requestData: {},
+      },
+    };
     const res = {
       data: {},
       code: 0,
 
-      status: function(status) {
-        this.code = status
+      status: function (status) {
+        this.code = status;
       },
-      send: function(input) {
-        this.data = input
-      }
-    }
+      send: function (input) {
+        this.data = input;
+      },
+    };
 
-    handleGetHistory(req, res)
+    handleNewQuote(req, res);
+
+    expect(res.data.error).toEqual("User ID not provided in the request.");
+  });
+
+  test("Test New Quote - no body", () => {
+    const req = {};
+    const res = {
+      data: {},
+      code: 0,
+
+      status: function (status) {
+        this.code = status;
+      },
+      send: function (input) {
+        this.data = input;
+      },
+    };
+
+    handleNewQuote(req, res);
+
+    expect(res.data.error).toEqual("Internal server error");
+  });
+
+  test("Test Get Quote History - empty", () => {
+    const req = {};
+    const res = {
+      data: {},
+      code: 0,
+
+      status: function (status) {
+        this.code = status;
+      },
+      send: function (input) {
+        this.data = input;
+      },
+    };
+
+    handleGetHistory(req, res);
 
     const quotes = [
       {
-        text: "New Fuel"
-      }
-    ]
-    
-    expect(res.data).toEqual(quotes)
-  })
-}) 
+        userID: 1,
+        amount: 10,
+        unitPrice: 1,
+        deliveryDate: "2023-01-01",
+        mainAddress: "123 City St.",
+      },
+    ];
+
+    expect(res.data).toEqual(quotes);
+  });
+});
