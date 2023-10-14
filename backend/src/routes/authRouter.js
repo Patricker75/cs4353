ae9c81ef8be2fc12a734463c04d738912967e716
Patricker75 <biet.vui@gmail.com>
2023-10-13 20:52:53 -0500

    moved functions into handler file

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,63 +1,16 @@
 // authRouter.js
-const express = require('express');
-const router = express.Router();
+import { Router } from 'express';
+import { handleLogin, handleLogout, handleStatus } from '../handlers/authHandler';
 
-// Simulated database for storing user information
-const users = {
-  'john.doe@example.com': {
-    id: 1,
-    name: 'John Doe',
-    password: 'password123',
-  },
-};
+const router = Router();
 
 // Create a route to handle user login (POST)
-router.post('/api/auth/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = users[email];
-  if (!user) {
-    res.status(401).json({ message: 'Invalid email or password' });
-    return;
-  }
-
-  if (password !== user.password) {
-    res.status(401).json({ message: 'Invalid email or password' });
-    return;
-  }
-
-  // Set the user ID in the request object
-  req.user = user.id;
-
-  // Send a success response
-  res.status(200).json({ message: 'Login successful' });
-});
+router.post('/api/auth/login', handleLogin);
 
 // Create a route to handle user logout (POST)
-router.post('/api/auth/logout', async (req, res) => {
-  // Clear the user ID in the request object
-  req.user = undefined;
-
-  // Send a success response
-  res.status(200).json({ message: 'Logout successful' });
-});
+router.post('/api/auth/logout', handleLogout);
 
 // Create a route to get the current user's status (GET)
-router.get('/api/auth/status', async (req, res) => {
-  // Get the user ID from the request object
-  const userId = req.user;
+router.get('/api/auth/status', handleStatus);
 
-  // Check if the user is logged in
-  if (!userId) {
-    res.status(401).json({ message: 'User is not logged in' });
-    return;
-  }
-
-  // Get the user's profile from the database
-  const user = users[userId];
-
-  // Send the user's profile in the response
-  res.status(200).json(user);
-});
-
-module.exports = router;
+export default router;
--- backend/src/routes/quotesRouter.js
+++ backend/src/routes/quotesRouter.js
@@ -1,26 +1,13 @@
 // quotesRouter.js
-const express = require('express');
-const router = express.Router();
+import { Router } from 'express';
+import { handleNewQuote, handleGetHistory } from '../handlers/quotesHandler';
 
-// Simulated database for storing fuel quotes
-const quotes = [];
+const router = Router();
 
 // Create a route to create a new fuel quote (POST)
-router.post('/api/quotes/new', async (req, res) => {
-  // Get the fuel quote data from the request body
-  const quoteData = req.body;
-
-  // Add the new fuel quote to the database
-  quotes.push(quoteData);
-
-  // Send a success response
-  res.status(200).json({ message: 'New fuel quote created' });
-});
+router.post('/api/quotes/new', handleNewQuote);
 
 // Create a route to retrieve fuel quote history (GET)
-router.get('/api/quotes/history', async (req, res) => {
-  // Send the fuel quote history in the response
-  res.status(200).json(quotes);
-});
+router.get('/api/quotes/history', handleGetHistory);
 
-module.exports = router;
+export default router;
