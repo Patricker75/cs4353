de66ec345bdd92a5fbbb88244f5ee37c8f6fb152
Patricker75 <biet.vui@gmail.com>
2023-11-26 15:34:49 -0600

    added pricing module to route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,13 @@
 import loginRouter from "./loginRouter";
 import profileRouter from "./profileRouter";
 import quotesRouter from "./quotesRouter";
+import pricingRouter from "./pricingRouter";
 import testRouter from "./testRouter";
 
-export default [loginRouter, profileRouter, quotesRouter, testRouter];
+export default [
+  loginRouter,
+  profileRouter,
+  quotesRouter,
+  pricingRouter,
+  testRouter,
+];
