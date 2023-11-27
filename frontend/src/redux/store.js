41d5f2cbd1e8aa7ab17cacf81ab87b46c8de57b9
Patricker75 <biet.vui@gmail.com>
2023-11-27 17:35:59 -0600

    created a profile slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,25 +1,19 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
 import fuelQuoteSliceReducer from "./slices/fuelQuoteSlice";
-import clientProfileReducer from "./slices/clientProfileSlice";
 import historyReducer from "./slices/historySlice"; // Import the history slice
 import authReducer from "./slices/authSlice";
+import profileReducer from "./slices/profileSlice";
 
 const rootReducer = {
   fuelQuote: fuelQuoteSliceReducer,
-  clientProfile: clientProfileReducer,
   history: historyReducer, // Include the history slice
   auth: authReducer,
+  profile: profileReducer,
 };
 
 const store = configureStore({
   reducer: rootReducer,
-});
-
-// Subscribe to the store and log the state whenever it changes
-store.subscribe(() => {
-  const state = store.getState();
-  console.log("Redux Store State:", state);
 });
 
 export default store;
