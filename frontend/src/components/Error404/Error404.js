8f25087f7d4c13f4588d79176f1a3303e4d81e83
Hiep Tran <60906609+Hiept152@users.noreply.github.com>
2023-12-01 23:59:22 -0600

    modified 'Error404.js'

--- frontend/src/components/Error404/Error404.js
+++ frontend/src/components/Error404/Error404.js
@@ -6,8 +6,8 @@ const Error404 = () => {
   return (
     <div>
       <h1 className={styles["page-header"]}>404 - Page Not Found</h1>
-      <p className={styles["parar"]}>The page you're looking for doesn't exist.</p>
       <p className={styles["parar"]}>Aww, going to cry little baby?</p>
+      <p className={styles["parar"]}>The page you're looking for doesn't exist.</p>
       <Link className={styles["button-home"]} to="/">
             <button>Go Back to Home</button>
         </Link>
